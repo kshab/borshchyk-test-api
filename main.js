89ea4b3b7261b327c7136de4db45f33f2347a500
Konstantin Shabaliev <kvshabalev@gmail.com>
2024-02-26 18:53:27 +0200

    fix post route

--- main.js
+++ main.js
@@ -11,15 +11,15 @@ app.use(bodyParser.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', async (req, res) => 
+app.get('/', async (req, res) =>  {
   console.log('Hello');
   res.status(200).json({ created: 'test' });
 });
 
 app.post('/order', async (req, res) => {
-  const { item } = req.body;
-  console.log(`${req.socket.remoteAddress} POST /order ${item}`);
-  res.status(201).json({ created: item });
+  const { order } = req.body;
+  console.log(`${req.socket.remoteAddress} POST /order ${order}`);
+  res.status(201).json({ created: order });
 });
 
 app.listen(PORT, () => {
