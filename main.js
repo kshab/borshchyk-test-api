35dc784f3c78ef6be1e2d6207cc6931a208671ab
Konstantin Shabaliev <kvshabalev@gmail.com>
2024-02-26 18:33:06 +0200

    init app

--- main.js
+++ main.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const PORT = 8000;
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post('/order', async (req, res) => {
+  const { item } = req.body;
+  console.log(`${req.socket.remoteAddress} POST /order ${item}`);
+  res.status(201).json({ created: item });
+});
+
+app.listen(PORT, () => {
+  console.log(`Listen on port ${PORT}`);
+});
